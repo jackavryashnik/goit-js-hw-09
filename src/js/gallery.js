dde11b40b10e603ad13387b5177d24fd25da511d
jackavryashnik <jackavryashnik@gmail.com>
2023-12-04 22:41:17 +0200

    change derectory

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -5,48 +5,48 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const images = [
   {
-    preview: './img/1.jpg',
-    original: './img/1@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/1.jpg',
+    original: '/goit-js-hw-09/src/img/1@x3.jpg',
     description: 'developing strategy process',
   },
   {
-    preview: './img/2.jpg',
-    original: './img/2@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/2.jpg',
+    original: '/goit-js-hw-09/src/img/2@x3.jpg',
     description: 'light bulb symbolize idea',
   },
   {
-    preview: './img/3.jpg',
-    original: './img/3@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/3.jpg',
+    original: '/goit-js-hw-09/src/img/3@x3.jpg',
     description: 'smartphone with slogan on display',
   },
   {
-    preview: './img/4.jpg',
-    original: './img/4@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/4.jpg',
+    original: '/goit-js-hw-09/src/img/4@x3.jpg',
     description: 'meeting room',
   },
   {
-    preview: './img/5.jpg',
-    original: './img/5@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/5.jpg',
+    original: '/goit-js-hw-09/src/img/5@x3.jpg',
     description: 'rest zone',
   },
   {
-    preview: './img/6.jpg',
-    original: './img/6@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/6.jpg',
+    original: '/goit-js-hw-09/src/img/6@x3.jpg',
     description: 'coworking space with full seats',
   },
   {
-    preview: './img/7.jpg',
-    original: './img/7@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/7.jpg',
+    original: '/goit-js-hw-09/src/img/7@x3.jpg',
     description: 'laptop and coffee',
   },
   {
-    preview: './img/8.jpg',
-    original: './img/8@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/8.jpg',
+    original: '/goit-js-hw-09/src/img/8@x3.jpg',
     description: 'person working on laptop',
   },
   {
-    preview: './img/9.jpg',
-    original: './img/9@x3.jpg',
+    preview: '/goit-js-hw-09/src/img/9.jpg',
+    original: '/goit-js-hw-09/src/img/9@x3.jpg',
     description: 'meeting',
   },
 ];
